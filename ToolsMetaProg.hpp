ef7c5dd52878e5dc22e2a6f8b848d2109e1b10d3
lemoinep <patrick.lemoine@cemosis.fr>
2024-02-16 11:55:18 +0100

    Update code benchmark

--- ToolsMetaProg.hpp
+++ ToolsMetaProg.hpp
@@ -421,4 +421,49 @@ void write_tuple(const std::tuple<Args...>& t)
 
 //================================================================================================================================
 
+template<class Function, class... Args>
+void async_wrapper(Function&& f, Args&&... args, std::future<void>& future,
+                   std::future<void>&& is_valid, std::promise<void>&& is_moved) {
+    is_valid.wait(); // Wait until the return value of std::async is written to "future"
+    auto our_future = std::move(future); // Move "future" to a local variable
+    is_moved.set_value(); // Only now we can leave void_async in the main thread
+
+    // This is also used by std::async so that member function pointers work transparently
+    auto functor = std::bind(f, std::forward<Args>(args)...);
+    functor();
+}
+
+template<class Function, class... Args> // This is what you call instead of std::async
+void void_async(Function&& f, Args&&... args) {
+    std::future<void> future; // This is for std::async return value
+    // This is for our synchronization of moving "future" between threads
+    std::promise<void> valid;
+    std::promise<void> is_moved;
+    auto valid_future = valid.get_future();
+    auto moved_future = is_moved.get_future();
+
+    // Here we pass "future" as a reference, so that async_wrapper
+    // can later work with std::async's return value
+    future = std::async(
+        async_wrapper<Function, Args...>,
+        std::forward<Function>(f), std::forward<Args>(args)...,
+        std::ref(future), std::move(valid_future), std::move(is_moved)
+    );
+    valid.set_value(); // Unblock async_wrapper waiting for "future" to become valid
+    moved_future.wait(); // Wait for "future" to actually be moved
+}
+
+
+int long_running_task(int target, const std::atomic_bool& cancelled)
+{
+    // simulate a long running task for target*100ms, 
+    // the task should check for cancelled often enough!
+    while(target-- && !cancelled)
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    // return results to the future or raise an error 
+    // in case of cancellation
+    return cancelled ? 1 : 0;
+}
+
+//================================================================================================================================
 
